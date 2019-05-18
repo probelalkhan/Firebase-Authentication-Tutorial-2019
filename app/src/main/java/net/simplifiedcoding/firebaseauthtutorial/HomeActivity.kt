111df712a4a2e4ad396b31e56ed7924aaf757ccf
Belal Khan <probelalkhan@gmail.com>
2019-05-18 15:49:08 +0900

    Navigation Drawer

--- app/src/main/java/net/simplifiedcoding/firebaseauthtutorial/HomeActivity.kt
+++ app/src/main/java/net/simplifiedcoding/firebaseauthtutorial/HomeActivity.kt
@@ -2,14 +2,38 @@ package net.simplifiedcoding.firebaseauthtutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_home.*
+
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(toolbar)
+
+        val navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupWithNavController(nav_view, navController)
+        NavigationUI.setupActionBarWithNavController(
+            this,
+            navController, drawer_layout
+        )
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.fragment),
+            drawer_layout
+        )
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
 
 }
