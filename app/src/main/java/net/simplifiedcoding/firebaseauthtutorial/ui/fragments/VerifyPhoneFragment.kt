ec382a3979fe07bd26dc53c734d98cab7e63158b
Belal Khan <probelalkhan@gmail.com>
2019-05-26 23:56:46 +0900

    Phone Authentication

--- app/src/main/java/net/simplifiedcoding/firebaseauthtutorial/ui/fragments/VerifyPhoneFragment.kt
+++ app/src/main/java/net/simplifiedcoding/firebaseauthtutorial/ui/fragments/VerifyPhoneFragment.kt
@@ -6,12 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
 import kotlinx.android.synthetic.main.fragment_verify_phone.*
 
 import net.simplifiedcoding.firebaseauthtutorial.R
+import net.simplifiedcoding.firebaseauthtutorial.utils.toast
+import java.util.concurrent.TimeUnit
 
 
 class VerifyPhoneFragment : Fragment() {
+
+    private var verificationId : String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,10 +39,75 @@ class VerifyPhoneFragment : Fragment() {
 
         button_send_verification.setOnClickListener {
 
+            val phone = edit_text_phone.text.toString().trim()
+
+            if (phone.isEmpty() || phone.length != 10) {
+                edit_text_phone.error = "Enter a valid phone"
+                edit_text_phone.requestFocus()
+                return@setOnClickListener
+            }
+
+            val phoneNumber = '+' + ccp.selectedCountryCode + phone
+
+            PhoneAuthProvider.getInstance()
+                .verifyPhoneNumber(
+                    phoneNumber,
+                    60,
+                    TimeUnit.SECONDS,
+                    requireActivity(),
+                    phoneAuthCallbacks
+                )
+
 
             layoutPhone.visibility = View.GONE
             layoutVerification.visibility = View.VISIBLE
         }
+
+        button_verify.setOnClickListener {
+            val code = edit_text_code.text.toString().trim()
+
+            if(code.isEmpty()){
+                edit_text_code.error = "Code required"
+                edit_text_code.requestFocus()
+                return@setOnClickListener
+            }
+
+            verificationId?.let{
+                val credential = PhoneAuthProvider.getCredential(it, code)
+                addPhoneNumber(credential)
+            }
+        }
     }
 
+
+    private val phoneAuthCallbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        override fun onVerificationCompleted(phoneAuthCredential: PhoneAuthCredential?) {
+            phoneAuthCredential?.let {
+                addPhoneNumber(phoneAuthCredential)
+            }
+        }
+
+        override fun onVerificationFailed(exception: FirebaseException?) {
+            context?.toast(exception?.message!!)
+        }
+
+        override fun onCodeSent(verificationId: String?, token: PhoneAuthProvider.ForceResendingToken?) {
+            super.onCodeSent(verificationId, token)
+            this@VerifyPhoneFragment.verificationId = verificationId
+        }
+    }
+
+    private fun addPhoneNumber(phoneAuthCredential: PhoneAuthCredential) {
+        FirebaseAuth.getInstance()
+            .currentUser?.updatePhoneNumber(phoneAuthCredential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    context?.toast("Phone Added")
+                    val action = VerifyPhoneFragmentDirections.actionPhoneVerified()
+                    Navigation.findNavController(button_verify).navigate(action)
+                } else {
+                    context?.toast(task.exception?.message!!)
+                }
+            }
+    }
 }
